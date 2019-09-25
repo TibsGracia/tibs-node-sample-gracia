a5968617a5590a92b4d7adb470a3d0f9daef39f2
kind-soul07 <ziekind07.com>
2019-09-25 14:33:13 +0800

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express');
 var nicknames = {}
-var port = process.env.port||5000
+var port = process.env.PORT||5000
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
